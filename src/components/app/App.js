ca1ba2934ff084f70f66387e29b53ebee5207b68
Senya192 <andreyzhitomir1707@gmail.com>
2020-07-20 20:19:14 +0300

    h2 in App

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -7,32 +7,32 @@ import NavBar from "../nav-bar";
 import ListsContainer from "../lists-container";
 
 function App() {
-  const [menu, setMenu] = useState();
-  useEffect(() => {
-    const fetchAPI = async () => {
-      setMenu(await fetchRestoran());
-    };
-    fetchAPI();
-  }, []);
-  const restName = menu ? menu.data.restaurant.company.name : null;
-  const restMenus = menu ? menu.data.restaurant.menu.categories : null;
-  console.log(restMenus);
-  return (
-    <Router>
-      <NavBar title={restName}></NavBar>
-      <Switch>
-        <Route path="/info" exact>
-          <h2>Тут будет информация</h2>
-        </Route>
-        <Route path="/corzina" exact>
-          <h2>Тут будет корзина</h2>
-        </Route>
-        <Route path="/" exact >
-          <ListsContainer restMenus={restMenus}/>
-        </Route>
-      </Switch>
-    </Router>
-  );
+	const [menu, setMenu] = useState();
+	useEffect(() => {
+		const fetchAPI = async () => {
+			setMenu(await fetchRestoran());
+		};
+		fetchAPI();
+	}, []);
+	const restName = menu ? menu.data.restaurant.company.name : null;
+	const restMenus = menu ? menu.data.restaurant.menu.categories : null;
+	console.log(restMenus);
+	return (
+		<Router>
+			<NavBar title={restName}></NavBar>
+			<Switch>
+				<Route path="/info" exact>
+					<h2>123456789</h2>
+				</Route>
+				<Route path="/corzina" exact>
+					<h2>Тут будет корзина</h2>
+				</Route>
+				<Route path="/" exact >
+					<ListsContainer restMenus={restMenus} />
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
